7325ce3ee8b0f31edfd5c85a9b895842facd64eb
MenZil <menzil@live.cn>
2015-10-29 12:24:26 +0800

    switch,case和break 的用法：输入分数输出成绩A，B，C 等

---
+++ students-score.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+/*
+给出一百分制成绩，要求输出成绩等级'A','B','C','D','E'。90分以上为 A，
+80~89分为 B,70~79分为 C，60~69分为 D，60分以下为 E。
+*/
+int main(int argc, char const *argv[])
+{
+	float score;
+	char grade;
+	printf("请输入学生成绩:\n");
+	scanf("%f",&score);
+	while(score > 100 || score < 0) {
+		printf("\n 输入有误，请重输:");
+		scanf("%f",&score);
+	}
+	switch ((int)(score / 10)){
+		case 10:
+		case 9: grade = 'A';
+		break;
+		case 8: grade = 'B';
+		break;
+		case 7: grade = 'C';
+		break;
+		case 6: grade = 'D';
+		break;
+		case 5:
+		case 4:
+		case 3:
+		case 2:
+		case 1:
+		case 0: grade = 'E';
+	}
+	printf("成绩是 %5.1f,相应等级是 %c\n",score,grade );	
+	return 0;
+}
